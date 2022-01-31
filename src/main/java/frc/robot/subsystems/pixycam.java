876d9bd92a291c981ac597344137c7c5cf0aa091
Eagle Engineering <1297591536@qq.com>
2022-01-30 16:41:25 -0800

    nothing

---
+++ src/main/java/frc/robot/subsystems/pixycam.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.*;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class pixycam extends SubsystemBase {
+    
+}
