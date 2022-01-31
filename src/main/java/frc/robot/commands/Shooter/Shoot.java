771902c1f49eed736ef93cc2082af86d0964ded6
PikablockPullRequest <snicholls313@icloud.com>
2022-01-30 16:27:54 -0800

    Included one more line
    Not important but my ocd says yes so here it is fhhfshgshgshgsshgsfhgshg

--- src/main/java/frc/robot/commands/Shooter/Shoot.java
+++ src/main/java/frc/robot/commands/Shooter/Shoot.java
@@ -25,7 +25,7 @@ public class Shoot extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.move(-KIntakePWM);
+    shooter.move(-KIntakeSpinPWM);
   }
 
   // Called once the command ends or is interrupted.
