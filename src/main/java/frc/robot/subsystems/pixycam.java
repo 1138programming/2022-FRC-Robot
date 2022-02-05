00a71fa37b166a53fedabcc20d4d0664e007c3f1
Patrick62 <90010357+WillSeguin@users.noreply.github.com>
2022-02-05 11:56:49 -0800

    Added Repository So Pixy2 Code would work

--- src/main/java/frc/robot/subsystems/pixycam.java
+++ src/main/java/frc/robot/subsystems/pixycam.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+
 
 public class pixycam extends SubsystemBase {
-    
+
 }
