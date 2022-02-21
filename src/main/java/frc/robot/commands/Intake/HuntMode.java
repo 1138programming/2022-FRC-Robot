b796cdb662825654a0d13b9b4afbaece4263d5ea
Patrick62 <90010357+WillSeguin@users.noreply.github.com>
2022-02-21 13:57:47 -0800

    Added Comments to HuntMode

--- src/main/java/frc/robot/commands/Intake/HuntMode.java
+++ src/main/java/frc/robot/commands/Intake/HuntMode.java
@@ -20,7 +20,7 @@ public class HuntMode extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.swivelToPos(KIntakeAngle);
+    intake.swivelToPos(KIntakeAngle); // Sets the intake to hunt mode.
     SmartDashboard.putBoolean("Pixy", false);
   }
 
@@ -28,21 +28,44 @@ public class HuntMode extends CommandBase {
   @Override
   public void execute() {
     boolean pixySees = SmartDashboard.getBoolean("Pixy", false);
-    if (intake.getPixyColorRed() == 3 || intake.getPixyColorBlue() == 3 || pixySees == true)
-    {
-      intake.moveSwivel(KIntakeSwivelPWM);
-      intake.moveSpin(KIntakeSpinPWM);
+  if (intake.getIntakeEncoderDeg() == KIntakeAngle) //Checks to see if the the intake is at the correct angle.
+    {  
+      if (intake.getPixyColorRed() == 3 || intake.getPixyColorBlue() == 3) 
+      /* Checks to see if the ball is in the intake by adding the bumper objects to the total count of objects. 
+         Then it caps the amount of objects it will register to 3, making it so the code does not need to be switched before the match.
+      */
+        {
+          intake.swivelToPos(10);
+          intake.moveSpin(KIntakeSpinPWM); 
+          // This swivels the intake to the collection position and spins the intake motor.
+        }
+      else
+      {
+        intake.swivelToPos(KIntakeAngle);
+        intake.moveSpin(0);
+        // This keeps the intake in the hunt postion and stops the motor.
+      }
+      if (intake.getBottomLimitSwitch()) 
+      {
+        intake.moveSwivel(0);
+        // Stops the intakes when it gets to the limit switch. This is a failsafe if the intake starts to go too far down. 
+      }
     }
-    else
+  else
     {
-      intake.swivelToPos(KIntakeAngle);
-      intake.moveSpin(0);
+      if (intake.getIntakeEncoderDeg() <= KIntakeAngle && (intake.getPixyColorRed() == 3 || intake.getPixyColorBlue() == 3))
+        // Checks to see if the pixy still sees the ball and if the intake is below hunt mode angle.
+       {
+        intake.moveSpin(KIntakeSpinPWM);
+        // Spins the intake.
+       }
+      else
+       {
+         intake.swivelToPos(KIntakeAngle);
+         // Moves the intake back to hunt mode. 
+       }
+
     }
-    if (intake.getBottomLimitSwitch()) 
-    {
-      intake.moveSwivel(0);
-    }
-    
   }
 
   // Called once the command ends or is interrupted.
